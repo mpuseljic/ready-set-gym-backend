13a9f4aa236429a5f578113807434f14cd70c3dd
Mirna Pušeljić <puseljicmirna@gmail.com>
2024-01-11 21:39:23 +0100

    edit function for jwt registration

--- src/auth.js
+++ src/auth.js
@@ -10,12 +10,20 @@ usersCollection.createIndex({ email: 1 }, { unique: true });
 export default {
   async registerUser(userData) {
     const hashedPassword = await bcrypt.hash(userData.password, 10);
-    await usersCollection.insertOne({
-      _id: new ObjectId(),
-      firstName: userData.firstName,
-      lastName: userData.lastName,
-      email: userData.email,
-      password: hashedPassword,
-    });
+    try {
+      await usersCollection.insertOne({
+        _id: new ObjectId(),
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        password: hashedPassword,
+      });
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new Error("Email already exists. Please choose a different one.");
+      } else {
+        throw error;
+      }
+    }
   },
 };
