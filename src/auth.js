f99d9cf2c3ed1796ca8dc1801a344b586854e9e0
Luka Benković <benkovi.luka3@gmail.com>
2024-02-08 23:18:34 +0100

    dodavanje slika za profil, firebase storage

--- src/auth.js
+++ src/auth.js
@@ -9,109 +9,135 @@ const usersCollection = db.collection("Users");
 usersCollection.createIndex({ email: 1 }, { unique: true });
 
 export default {
-  async registerUser(userData) {
-    const hashedPassword = await bcrypt.hash(userData.password, 10);
-    try {
-      await usersCollection.insertOne({
-        _id: new ObjectId(),
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        email: userData.email,
-        password: hashedPassword,
-      });
-    } catch (error) {
-      if (error.code === 11000) {
-        throw new Error("Email already exists. Please choose a different one.");
-      } else {
-        throw error;
-      }
-    }
-  },
+    async registerUser(userData) {
+        const hashedPassword = await bcrypt.hash(userData.password, 10);
+        try {
+            await usersCollection.insertOne({
+                _id: new ObjectId(),
+                firstName: userData.firstName,
+                lastName: userData.lastName,
+                email: userData.email,
+                password: hashedPassword,
+            });
+        } catch (error) {
+            if (error.code === 11000) {
+                throw new Error(
+                    "Email already exists. Please choose a different one."
+                );
+            } else {
+                throw error;
+            }
+        }
+    },
 
-  async authenticateUser(email, password) {
-    let userData = await usersCollection.findOne({ email: email });
+    async authenticateUser(email, password) {
+        let userData = await usersCollection.findOne({ email: email });
 
-    console.log("Received data: ", email, password);
-    console.log("User data from the database: ", userData);
+        console.log("Received data: ", email, password);
+        console.log("User data from the database: ", userData);
 
-    if (
-      userData &&
-      userData.password &&
-      (await bcrypt.compare(password, userData.password))
+        if (
+            userData &&
+            userData.password &&
+            (await bcrypt.compare(password, userData.password))
+        ) {
+            delete userData.password;
+            let token = jwt.sign(userData, process.env.JWT_SECRET, {
+                algorithm: "HS512",
+                expiresIn: "1 week",
+            });
+            return {
+                token,
+                email: userData.email,
+            };
+        } else {
+            throw new Error("Cannot authenticate");
+        }
+    },
+
+    verify(req, res, next) {
+        try {
+            let authorization = req.headers.authorization.split(" ");
+            let type = authorization[0];
+            let token = authorization[1];
+
+            if (type !== "Bearer") {
+                return res.status(401).send();
+            } else {
+                req.jwt = jwt.verify(token, process.env.JWT_SECRET);
+                return next();
+            }
+        } catch (error) {
+            return res.status(401).send();
+        }
+    },
+
+    async changeUserProfile(
+        email,
+        firstName,
+        lastName,
+        old_password,
+        new_password
     ) {
-      delete userData.password;
-      let token = jwt.sign(userData, process.env.JWT_SECRET, {
-        algorithm: "HS512",
-        expiresIn: "1 week",
-      });
-      return {
-        token,
-        email: userData.email,
-      };
-    } else {
-      throw new Error("Cannot authenticate");
-    }
-  },
+        try {
+            console.log("Updating profile for email:", email);
+            console.log("New profile data:", { firstName, lastName });
+            const user = await usersCollection.findOne({ email: email });
 
-  verify(req, res, next) {
-    try {
-      let authorization = req.headers.authorization.split(" ");
-      let type = authorization[0];
-      let token = authorization[1];
+            if (
+                user &&
+                user.password &&
+                (await bcrypt.compare(old_password, user.password))
+            ) {
+                console.log("Old password matches. Updating profile...");
+                const delta = {};
+                if (firstName) {
+                    delta.firstName = firstName;
+                }
+                if (lastName) {
+                    delta.lastName = lastName;
+                }
+                if (new_password) {
+                    const new_password_hashed = await bcrypt.hash(
+                        new_password,
+                        8
+                    );
+                    delta.password = new_password_hashed;
+                }
 
-      if (type !== "Bearer") {
-        return res.status(401).send();
-      } else {
-        req.jwt = jwt.verify(token, process.env.JWT_SECRET);
-        return next();
-      }
-    } catch (error) {
-      return res.status(401).send();
-    }
-  },
+                const result = await usersCollection.updateOne(
+                    { _id: user._id },
+                    { $set: delta }
+                );
 
-  async changeUserProfile(
-    email,
-    firstName,
-    lastName,
-    old_password,
-    new_password
-  ) {
-    try {
-      console.log("Updating profile for email:", email);
-      console.log("New profile data:", { firstName, lastName });
-      const user = await usersCollection.findOne({ email: email });
-
-      if (
-        user &&
-        user.password &&
-        (await bcrypt.compare(old_password, user.password))
-      ) {
-        console.log("Old password matches. Updating profile...");
-        const delta = {};
-        if (firstName) {
-          delta.firstName = firstName;
+                return result.modifiedCount === 1;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error(error);
+            throw new Error("Failed to change password!");
         }
-        if (lastName) {
-          delta.lastName = lastName;
-        }
-        if (new_password) {
-          const new_password_hashed = await bcrypt.hash(new_password, 8);
-          delta.password = new_password_hashed;
-        }
+    },
+    async changeUserProfilePicture(email, imagePath) {
+        try {
+            console.log("Updating profile for email:", email);
+            const user = await usersCollection.findOne({ email: email });
 
-        const result = await usersCollection.updateOne(
-          { _id: user._id },
-          { $set: delta }
-        );
+            if (user && user.password) {
+                const delta = { imagePath: imagePath };
+                const result = await usersCollection.updateOne(
+                    { _id: user._id },
+                    { $set: delta }
+                );
 
-        return result.modifiedCount === 1;
-      } else {
-        return false;
-      }
-    } catch (error) {
-      console.error(error);
-      throw new Error("Failed to change password!");
-    }
-  },
+                return result.modifiedCount === 1;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error(error);
+            throw new Error("Failed to change password!");
+        }
+    },
 };
--- src/index.js
+++ src/index.js
@@ -15,185 +15,198 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/secure-route", [auth.verify], (req, res) => {
-  res.json({ message: "This is a secure route" });
+    res.json({ message: "This is a secure route" });
 });
 
 app.post("/auth", async (req, res) => {
-  let userData = req.body;
-  try {
-    const respData = await auth.authenticateUser(
-      userData.email,
-      userData.password
-    );
-    res.json({ token: respData.token });
-  } catch (error) {
-    res.status(403).json({ error: error.message });
-  }
+    let userData = req.body;
+    try {
+        const respData = await auth.authenticateUser(
+            userData.email,
+            userData.password
+        );
+        res.json({ token: respData.token });
+    } catch (error) {
+        res.status(403).json({ error: error.message });
+    }
 });
 
 app.post("/users", async (req, res) => {
-  const userData = req.body;
-  try {
-    const respData = await auth.registerUser(userData);
-    res.json(respData);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Email already exists. Please choose a different one." });
-  }
+    const userData = req.body;
+    try {
+        const respData = await auth.registerUser(userData);
+        res.json(respData);
+    } catch (error) {
+        res.status(500).json({
+            error: "Email already exists. Please choose a different one.",
+        });
+    }
 });
 
 app.get("/users/profile", [auth.verify], async (req, res) => {
-  try {
-    const user = await usersCollection.findOne({ email: req.jwt.email });
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+    try {
+        const user = await usersCollection.findOne({ email: req.jwt.email });
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 app.patch("/user", [auth.verify], async (req, res) => {
-  try {
-    const userData = req.body;
-    const email = req.jwt.email;
-    const result = await auth.changeUserProfile(
-      email,
-      userData.firstName,
-      userData.lastName,
-      userData.old_password,
-      userData.new_password
-    );
+    try {
+        const userData = req.body;
+        const email = req.jwt.email;
+        let result;
+        if (userData.new_password) {
+            result = await auth.changeUserProfile(
+                email,
+                userData.firstName,
+                userData.lastName,
+                userData.old_password,
+                userData.new_password
+            );
+        } else {
+            result = await auth.changeUserProfilePicture(
+                email,
+                userData.imagePath
+            );
+        }
 
-    if (result) {
-      console.log("Profile successfully updated.");
-      res.status(201).json({ message: "Password successfully updated." });
-    } else {
-      console.log("Failed to update profile.");
-      res.status(500).json({ error: "Failed to update password." });
+        if (result) {
+            console.log("Profile successfully updated.");
+            res.status(201).json({ message: "Password successfully updated." });
+        } else {
+            console.log("Failed to update profile.");
+            res.status(500).json({ error: "Failed to update password." });
+        }
+    } catch (error) {
+        console.error("Error updating profile:", error);
+        res.status(500).json({ error: "Failed to update password!" });
     }
-  } catch (error) {
-    console.error("Error updating profile:", error);
-    res.status(500).json({ error: "Failed to update password!" });
-  }
 });
 
 app.get("/exercises", async (req, res) => {
-  try {
-    const exerciseList = await db.collection("exerciselist").find({}).toArray();
-    res.json(exerciseList);
-  } catch (error) {
-    console.error("Error fetching exercise list:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+    try {
+        const exerciseList = await db
+            .collection("exerciselist")
+            .find({})
+            .toArray();
+        res.json(exerciseList);
+    } catch (error) {
+        console.error("Error fetching exercise list:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 app.get("/recommendedworkouts/:workoutType", async (req, res) => {
-  const workoutType = req.params.workoutType;
-  try {
-    const recommendedWorkouts = await db
-      .collection("recommendedWorkouts")
-      .findOne({ type: workoutType });
-    res.json(recommendedWorkouts);
-  } catch (error) {
-    console.error("Error fetching recommended workouts:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+    const workoutType = req.params.workoutType;
+    try {
+        const recommendedWorkouts = await db
+            .collection("recommendedWorkouts")
+            .findOne({ type: workoutType });
+        res.json(recommendedWorkouts);
+    } catch (error) {
+        console.error("Error fetching recommended workouts:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 app.post("/diary/record", [auth.verify], async (req, res) => {
-  try {
-    const { content, date } = req.body;
-    const user = await usersCollection.findOne({ email: req.jwt.email });
+    try {
+        const { content, date } = req.body;
+        const user = await usersCollection.findOne({ email: req.jwt.email });
 
-    if (user) {
-      await db.collection("UserDiary").insertOne({
-        email: user.email,
-        content,
-        date,
-      });
-      res.status(200).json({ message: "Diary entry saved successfully." });
-    } else {
-      res.status(404).json({ error: "User not found." });
+        if (user) {
+            await db.collection("UserDiary").insertOne({
+                email: user.email,
+                content,
+                date,
+            });
+            res.status(200).json({
+                message: "Diary entry saved successfully.",
+            });
+        } else {
+            res.status(404).json({ error: "User not found." });
+        }
+    } catch (error) {
+        console.error("Error saving diary entry:", error);
+        res.status(500).json({ error: "Failed to save diary entry." });
     }
-  } catch (error) {
-    console.error("Error saving diary entry:", error);
-    res.status(500).json({ error: "Failed to save diary entry." });
-  }
 });
 
 app.get("/users/profile-diary", [auth.verify], async (req, res) => {
-  try {
-    const diaries = await db
-      .collection("UserDiary")
-      .find({ email: req.jwt.email })
-      .toArray();
-    res.json(diaries);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+    try {
+        const diaries = await db
+            .collection("UserDiary")
+            .find({ email: req.jwt.email })
+            .toArray();
+        res.json(diaries);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 app.delete("/diary/record/:diaryId", [auth.verify], async (req, res) => {
-  try {
-    const diaryId = req.params.diaryId;
+    try {
+        const diaryId = req.params.diaryId;
 
-    const user = await usersCollection.findOne({ email: req.jwt.email });
-    if (!user) {
-      return res.status(404).json({ error: "User not found." });
+        const user = await usersCollection.findOne({ email: req.jwt.email });
+        if (!user) {
+            return res.status(404).json({ error: "User not found." });
+        }
+
+        const diaryEntry = await db.collection("UserDiary").findOne({
+            _id: new mongo.ObjectId(diaryId),
+            email: user.email,
+        });
+        if (!diaryEntry) {
+            return res.status(404).json({ error: "Diary entry not found." });
+        }
+
+        await db.collection("UserDiary").deleteOne({
+            _id: new mongo.ObjectId(diaryId),
+        });
+        res.status(200).json({ message: "Diary entry deleted successfully." });
+    } catch (error) {
+        console.error("Error deleting diary entry:", error);
+        res.status(500).json({ error: "Failed to delete diary entry." });
     }
-
-    const diaryEntry = await db.collection("UserDiary").findOne({
-      _id: new mongo.ObjectId(diaryId),
-      email: user.email,
-    });
-    if (!diaryEntry) {
-      return res.status(404).json({ error: "Diary entry not found." });
-    }
-
-    await db.collection("UserDiary").deleteOne({
-      _id: new mongo.ObjectId(diaryId),
-    });
-    res.status(200).json({ message: "Diary entry deleted successfully." });
-  } catch (error) {
-    console.error("Error deleting diary entry:", error);
-    res.status(500).json({ error: "Failed to delete diary entry." });
-  }
 });
 
 app.post("/weight/record", [auth.verify], async (req, res) => {
-  try {
-    const { weight } = req.body;
-    const user = await usersCollection.findOne({ email: req.jwt.email });
+    try {
+        const { weight } = req.body;
+        const user = await usersCollection.findOne({ email: req.jwt.email });
 
-    if (user) {
-      await db.collection("Weight").insertOne({
-        email: user.email,
-        weight: parseInt(weight),
-        month: new Date().toLocaleString("en-US", { month: "long" }),
-      });
-      res.status(200).json({ message: "Weight recorded successfully." });
-    } else {
-      res.status(404).json({ error: "User not found." });
+        if (user) {
+            await db.collection("Weight").insertOne({
+                email: user.email,
+                weight: parseInt(weight),
+                month: new Date().toLocaleString("en-US", { month: "long" }),
+            });
+            res.status(200).json({ message: "Weight recorded successfully." });
+        } else {
+            res.status(404).json({ error: "User not found." });
+        }
+    } catch (error) {
+        console.error("Error recording weight:", error);
+        res.status(500).json({ error: "Failed to record weight." });
     }
-  } catch (error) {
-    console.error("Error recording weight:", error);
-    res.status(500).json({ error: "Failed to record weight." });
-  }
 });
 
 app.get("/weight", [auth.verify], async (req, res) => {
-  try {
-    const weights = await db
-      .collection("Weight")
-      .find({ email: req.jwt.email })
-      .toArray();
-    res.json({ weights });
-  } catch (error) {
-    console.error("Error fetching weights:", error);
-    res.status(500).json({ error: "Failed to fetch weights." });
-  }
+    try {
+        const weights = await db
+            .collection("Weight")
+            .find({ email: req.jwt.email })
+            .toArray();
+        res.json({ weights });
+    } catch (error) {
+        console.error("Error fetching weights:", error);
+        res.status(500).json({ error: "Failed to fetch weights." });
+    }
 });
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`));
