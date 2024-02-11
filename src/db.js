84cb1e8d4ce9789fa906312a2ef124eaf93c8872
Luka Benković <benkovi.luka3@gmail.com>
2024-02-11 16:04:25 +0100

    test vercel 4

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
 import { MongoClient } from "mongodb";
 const connectionString =
-  "mongodb+srv://mpuseljic:mirna123@cluster0.tjbw8it.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://mpuseljic:mirna123@cluster0.tjbw8it.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(connectionString);
 let conn = null;
 
 try {
-  console.log("Trying to establish connection...");
-  conn = await client.connect();
+    console.log("Trying to establish connection...");
+    conn = await client.connect();
 } catch (e) {
-  console.error(e);
+    console.error(e);
 }
 let db = conn.db("readySetGymDB");
 
